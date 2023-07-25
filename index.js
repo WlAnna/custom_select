463f4414698992060211b6e6d01c7b6dd38a7f55
“Anna <anna.wlodarczyk.dev@gmail.com>
2023-07-25 21:44:38 +0100

    Add EasyDropDown library

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log('aaa')
 
 let dropdownList = document.getElementById("list");
+let selected = document.getElementById("dropdown");
 dropdownList.style.display = "none";
 
 function openDropdown() {
@@ -20,9 +21,19 @@ for (let i = 0; i < currencyItemsLength; i++) {
         const select = document.querySelector('#dropdown');
         select.value = item.childNodes[3].innerHTML
 
+
+        // trigger onchange event
+        selected.selectedIndex = i;
+        selected.dispatchEvent(new Event('change', { bubbles: true }));
+
         let title = document.getElementById('dropdown-menu')
         title.innerHTML = `<span class="currency-icon ${item.childNodes[3].innerHTML}"></span>${item.childNodes[3].innerHTML}<span><i class="fa fa-solid fa-chevron-down"></i></span>`
 
         dropdownList.style.display = "none";
     })
 }
+
+// Easy dropdown library
+
+const selectElement = document.querySelector('#select');
+const instance = easydropdown(selectElement);
